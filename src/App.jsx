92e702d495bb8523e49cbfb9c2794ade41c1274e
Mina Kamali <minikamali1372.f@gmail.com>
2024-06-12 16:34:33 +0330

    Happy Birthday

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="bg">
+        <div className="container">
+          <p className="title">شما دعوتید به جشن تولد</p>
+          <p className="name">مینا و پویا</p>
+          <p className="body">قراراه حسابی بترکونیم</p>
+          <p className="date">روز پنجشنبه 31 خرداد</p>
+          <p className="time">از ساعت6 تا هر وقت از رقص سیر شیم</p>
+          <p className="address">آدرس منزلمون</p>
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
