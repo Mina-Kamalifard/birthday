e4ebbc9c16fd89703212c819170b75830d3b3d8e
Mina Kamali <minikamali1372.f@gmail.com>
2024-06-13 09:14:21 +0330

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ function App() {
         <div className="container">
           <p className="title">شما دعوتید به جشن تولد</p>
           <p className="name">مینا و پویا</p>
-          <p className="body">
-            قراره بنوشیم و برقصیم و بخندیم
-            <br /> و حسابی کنار هم بترکونیم
-          </p>
-          <p className="date">روز پنجشنبه 31 خرداد</p>
-          <p className="time">از ساعت6 تا پاسی از شب</p>
-          <p className="address">آدرس منزلمون</p>
-          <p></p>
+          <p className="body">قراره بنوشیم و برقصیم و بخندیم</p>
+          <p className="body2">روزمونو با حضورتون قشنگتر کنین</p>
+          <div>
+            <p className="date">روز پنجشنبه 31 خرداد</p>
+            <p className="time">از ساعت6 تا پاسی از شب</p>
+            <p className="address">آدرس منزلمون</p>
+          </div>
         </div>
       </div>
     </>
